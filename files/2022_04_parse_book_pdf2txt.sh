5db7dc6e435120a8142c85634a5c272a83c82c55
Christopher Meaney <christopher.meaney@utoronto.ca>
2022-04-03 10:17:48 -0400

    Update 2022_04_parse_book_pdf2txt.sh

--- files/2022_04_parse_book_pdf2txt.sh
+++ files/2022_04_parse_book_pdf2txt.sh
@@ -1,3 +1,9 @@
+###########################################
+## Parse PDF into txt and eventually into a structured "Question-Answer" dataset
+## From Kumar, Clark (2011). 1000 Clinical Questions Answered
+## https://med-mu.com/wp-content/uploads/2018/06/medsouls.blogspot.com-1000-Questions-and-Answers-from-Kumar-_-Clark_s-Clinical-Medicine-2e-Saunders-2011.pdf
+###########################################
+
 ## Updates
 sudo apt-get update
 
